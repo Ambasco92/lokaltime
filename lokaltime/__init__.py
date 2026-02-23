e8b865cfd30d5c9c6fd2a622bf61df366920e0dd
Abdul Basit Yakubu <142877178+Ambasco92@users.noreply.github.com>
2026-02-23 22:29:47 +0000

    Update __init__.py

--- lokaltime/__init__.py
+++ lokaltime/__init__.py
@@ -1,3 +1,9 @@
 from .date_formatter import DateFormatter
 from .date_parser import DateParser
 from .date_util import DateUtil
+
+__all__ = ['DateFormatter', 'DateParser', 'DateUtil']
+
+__version__ = "0.1.0"
+
+__author__ = "Abdul Basit Yakubu"
